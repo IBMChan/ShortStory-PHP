2a653c41ddbd8bc755bc246729bb3cf862e475db
Chandana G <chandana.g@ibm.com>
2025-08-29 15:19:14 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include "calc.php";
+
+echo "Variable declaration in PHP<br>";
+
+$id = 101;
+$name = "Sonu Joshi";
+$salary = 10.50;
+$married = false;
+
+// echo "$id <br>";
+// echo "$name <br>";
+// echo "$salary <br>";
+
+
+function hi() {
+    echo "Hi! What's up?";
+}
+
+hi();
+echo addNums(10,20)
+?>
+
+<!-- <?php
+echo "Hello, World!";
+
+?> -->
+
+
+<!-- mklink /J "C:\xampp\htdocs\ride_app" "D:\Projects\ibm-ui\php-demos\ride_app" -->
+
+<!-- ln -s /Volumes/D/Projects/ibm-ui/php-demos/ride_app /Applications/XAMPP/htdocs/ride_app -->
+
+<!-- http://localhost/demo_app/ -->
